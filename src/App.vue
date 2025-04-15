ec81611d00287369485662a6ad14ea53a84b3739
Darren Ling <yulung.ling@involve.asia>
2025-04-15 15:27:22 +0800

    second commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+
+  export default {
+    data() {
+      return {
+        test: "Testing 123"
+      }
+    }
+  }
+  
+</script>
+
+<template>
+  <h1>This is the initial to do list {{ test }}</h1>
+</template>
